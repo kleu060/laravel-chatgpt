d28ff4dc54bf3ebc7ea8f294fd3b310d42b842c6
Ka On Leung <kleu060@gmail.com>
2023-10-06 23:48:01 +1300

    update middleware in web.php

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,22 @@ Route::get('/', function () {
     return redirect(route("login"));
 });
 
-Route::get('/coverletter',[App\Http\Controllers\CoverLetterController::class, 'index'])->middleware(['auth', 'verified','checkChatGPT'])->name("CoverLetter");
 // Route::get('/coverletter', function () {
 //     return view('coverletter');
 // })->middleware(['auth', 'verified', 'checkChatGPT'])->name('CoverLetter');
 
-Route::post('/createPDF',[App\Http\Controllers\CoverLetterController::class, 'createPDF'])->middleware('checkChatGPT')->name("createPDF");
-Route::post('/generate', [App\Http\Controllers\ChatGPTController::class, 'generate'])->middleware('checkChatGPT')->name("generate");
-
-Route::get('/chat', [App\Http\Controllers\ChatGPTController::class, 'askToChatGpt'])->name("chat");
-Route::get('/list-letter', [App\Http\Controllers\LetterController::class, 'listLetter'])->name("ListLetter");
-Route::post('/save-letter', [App\Http\Controllers\LetterController::class, 'saveLetter'])->name("SaveLetter");
-Route::get('/view-letter/{id}', [App\Http\Controllers\LetterController::class, 'viewLetter'])->name("ViewLetter");
 
 
 
+Route::middleware(['auth', 'verified', 'checkChatGPT'])->group(function () {
+    Route::get('/coverletter',[App\Http\Controllers\CoverLetterController::class, 'index'])->name("CoverLetter");
+    Route::post('/createPDF',[App\Http\Controllers\CoverLetterController::class, 'createPDF'])->name("createPDF");
+    Route::post('/generate', [App\Http\Controllers\ChatGPTController::class, 'generate'])->name("generate");
+    Route::get('/chat', [App\Http\Controllers\ChatGPTController::class, 'askToChatGpt'])->name("chat");
+    Route::get('/list-letter', [App\Http\Controllers\LetterController::class, 'listLetter'])->name("ListLetter");
+    Route::post('/save-letter', [App\Http\Controllers\LetterController::class, 'saveLetter'])->name("SaveLetter");
+    Route::get('/view-letter/{id}', [App\Http\Controllers\LetterController::class, 'viewLetter'])->name("ViewLetter");
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
